d43365ec4aae8ec791adf112ea96a82d59446122
LazarevY <lazarev@cs.vsu.ru>
2021-03-09 21:53:21 +0300

    Fix mybatis conf and user model and mapper

--- src/main/java/app/data/mappers/UserMapper.java
+++ src/main/java/app/data/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package app.data.mappers;
 
 import app.data.modeles.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -19,5 +20,8 @@ public interface UserMapper {
 
     @Select("select * from users")
     List<User> findAll();
+
+    @Insert("INSERT INTO users (firstName, lastName, phone) VALUES (#{firstName}, #{lastName}, #{phone})")
+    void addUser(String firstName, String lastName, String phone);
 
 }
--- src/main/java/app/data/modeles/User.java
+++ src/main/java/app/data/modeles/User.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Data
 public class User {
     private Long id;
-    private String name;
+    private String firstName;
     private String lastName;
     private String phone;
 }
