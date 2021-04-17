c991049dd77cb49b1a1097cbf4cd2317c13b298d
LazarevY <lazarev@cs.vsu.ru>
2021-04-17 09:35:58 +0300

    Update paymethod mapper, add nested select into PPC mapper

--- src/main/java/app/data/mappers/PayMethodMapper.java
+++ src/main/java/app/data/mappers/PayMethodMapper.java
@@ -1,8 +1,18 @@
 package app.data.mappers;
 
+import app.data.modeles.PayMethod;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
 
 @Mapper
 public interface PayMethodMapper {
+    void addPayMethod(@Param("id") long id, @Param("userId") long userId,
+                      @Param("cardNumber") String cardNumber, @Param("dateEnd") Date dateEnd);
 
+    List<PayMethod> getAllByUserId(@Param("userId") long userId);
+
+    void deleteById(@Param("id") long id);
 }
--- src/main/java/app/data/mappers/ProductsProductsCategoriesMapper.java
+++ src/main/java/app/data/mappers/ProductsProductsCategoriesMapper.java
@@ -13,4 +13,7 @@ public interface ProductsProductsCategoriesMapper {
     List<Product> getProductsByCategory(@Param("id") long id);
     List<ProductCategory> getCategoriesOfProduct(@Param("id") long productId);
 
+    List<Product> getProductListByCategory(@Param("id") long id);
+    List<ProductCategory> getCategoryListOfProduct(@Param("id") long productId);
+
 }
