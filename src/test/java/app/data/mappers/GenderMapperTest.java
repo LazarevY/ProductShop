f7d3e015c8cf68242fd9b042fb437b3d965c50ef
LazarevY <lazarev@cs.vsu.ru>
2021-04-22 23:39:38 +0300

    Add gender mapper unit test

---
+++ src/test/java/app/data/mappers/GenderMapperTest.java
@@ -0,0 +1,78 @@
+package app.data.mappers;
+
+import app.data.modeles.Gender;
+import lombok.SneakyThrows;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.Reader;
+import java.util.stream.Collectors;
+
+
+@RunWith(SpringRunner.class)
+public class GenderMapperTest {
+
+    private static SqlSession session;
+
+    @TestConfiguration
+    static class GenderMapperConf {
+        @SneakyThrows
+        @Bean
+        public GenderMapper create() {
+            return session.getMapper(GenderMapper.class);
+        }
+    }
+
+    @Autowired
+    GenderMapper mapper;
+
+    @SneakyThrows
+    @BeforeClass
+    public static void setup() {
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+
+        SqlSessionFactory sessionFactory = builder.build(reader);
+        session = sessionFactory.openSession();
+    }
+
+    @Test
+    public void test000() {
+        mapper.addGender("TEST_GENDER");
+        var lst = mapper.getAll()
+                .stream()
+                .filter(gender -> gender.getName().equals("TEST_GENDER"))
+                .collect(Collectors.toList());
+        Assert.assertTrue(lst.size() > 0);
+
+        var a = mapper.getGender(lst.get(0).getId());
+        Assert.assertEquals(a.getName(), lst.get(0).getName());
+
+        for (var t : lst)
+            mapper.deleteById(t.getId());
+
+        lst = mapper.getAll()
+                .stream()
+                .filter(gender -> gender.getName().equals("TEST_GENDER"))
+                .collect(Collectors.toList());
+        Assert.assertTrue(lst.isEmpty());
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        session.close();
+    }
+
+}
