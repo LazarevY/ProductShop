300b9acb20ea96468c8ab1ff29b543ff7ca0e09f
LazarevY <lazarev@cs.vsu.ru>
2021-04-24 18:39:26 +0300

    Update product-store mapper and bean entity

--- src/main/java/app/data/mappers/ProductsStoresMapper.java
+++ src/main/java/app/data/mappers/ProductsStoresMapper.java
@@ -1,6 +1,7 @@
 package app.data.mappers;
 
 import app.data.modeles.Product;
+import app.data.modeles.ProductInStore;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,28 +11,41 @@ import java.util.List;
 @Mapper
 public interface ProductsStoresMapper {
 
-    List<Product> getAllInStore(@Param("storeId") long storeId);
+    void addProductToStore(@Param("storeId") long storeId,
+                           @Param("productId") long productId,
+                           @Param("count") int count,
+                           @Param("price") int price);
 
-    List<Product> getAllInStoreFilteredOnlyByPrice(
+    void updateProductData(@Param("storeId") long storeId,
+                           @Param("productId") long productId,
+                           @Param("newStoreId") long newStoreId,
+                           @Param("count") int count,
+                           @Param("price") int price);
+
+    List<ProductInStore> getAllInStore(@Param("storeId") long storeId);
+
+    List<ProductInStore> getAllInStoreFilteredOnlyByPrice(
             @Param("storeId") long storeId,
             @Param("priceLow") int priceLow,
             @Param("priceHigh") int priceHigh);
 
-    List<Product> getAllInStoreFilteredByPriceAndName(
+    List<ProductInStore> getAllInStoreFilteredByPriceAndName(
             @Param("storeId") long storeId,
             @Param("priceLow") int priceLow,
             @Param("priceHigh") int priceHigh,
             @Param("name") String name);
 
-    List<Product> getAllInStoreFilteredOnlyByCategories(
+    List<ProductInStore> getAllInStoreFilteredOnlyByCategories(
             @Param("storeId") long storeId,
             @Param("categoriesId") int[] categoriesId);
 
-    List<Product> getAllInStoreFilteredByPriceAndNameAndCategory(
+    List<ProductInStore> getAllInStoreFilteredByPriceAndNameAndCategory(
             @Param("storeId") long storeId,
             @Param("priceLow") int priceLow,
             @Param("priceHigh") int priceHigh,
             @Param("name") String name,
             @Param("categoriesId") int[] categoriesId);
+
+    void deleteByProductAndStoreId(@Param("productId") long productId, @Param("storeId") long storeId);
 
 }
