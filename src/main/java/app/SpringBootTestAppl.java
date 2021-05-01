4bf511bfa81ef8728ebf0a6441cfeb849afb2f2b
LazarevY <lazarev@cs.vsu.ru>
2021-05-01 13:38:47 +0300

    Update run class

--- src/main/java/app/SpringBootTestAppl.java
+++ src/main/java/app/SpringBootTestAppl.java
@@ -6,39 +6,17 @@ import app.data.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Lazarev Yaroslav
  */
 
 @SpringBootApplication
-public class SpringBootTestAppl implements CommandLineRunner {
+public class SpringBootTestAppl{
 
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private GenderMapper genderMapper;
-    @Autowired
-    private OrderProductMapper orderProductMapper;
-
-    @Override
-    public void run(String...args) {
-        var users = userMapper.findAll();
-        var genders = genderMapper.findAll();
-        var op = orderProductMapper.getOrderProduct(2L, 1L);
-        var orderProduct = orderProductMapper.getOrderByProduct(1L);
-
-        for (var u: users)
-            System.out.println(u.toString());
-        for (var u: genders)
-            System.out.println(u.toString());
-        for (var u: op)
-            System.out.println(u.toString());
-        for (var u: orderProduct)
-            System.out.println(u.toString());
-
-    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTestAppl.class, args);
     }
