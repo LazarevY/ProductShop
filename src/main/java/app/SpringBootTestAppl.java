ddefce5903c67050cdbfc8cc6ca2622e8a65197d
LazarevY <lazarev@cs.vsu.ru>
2021-05-01 13:40:53 +0300

    remove unneccessary import from run class

--- src/main/java/app/SpringBootTestAppl.java
+++ src/main/java/app/SpringBootTestAppl.java
@@ -1,14 +1,7 @@
 package app;
 
-import app.data.mappers.GenderMapper;
-import app.data.mappers.OrderProductMapper;
-import app.data.mappers.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Lazarev Yaroslav
