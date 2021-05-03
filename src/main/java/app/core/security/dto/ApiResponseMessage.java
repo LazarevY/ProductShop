7a1cc82429d367b10adbb71b32df07746e26452c
LazarevY <lazarev@cs.vsu.ru>
2021-05-03 15:57:17 +0300

    Update proj dep and security

--- src/main/java/app/core/security/dto/ApiResponseMessage.java
+++ src/main/java/app/core/security/dto/ApiResponseMessage.java
@@ -1,8 +1,10 @@
 package app.core.security.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ApiResponseMessage {
     private Boolean success;
     private String message;
